a0dd8ec7f12ac8a7478f12d9b6d74d43f27b71e6
richkash <akashincloud@gmail.com>
2025-08-24 18:41:47 -0400

    updated variables for AWS keys

--- provider.tf
+++ provider.tf
@@ -10,7 +10,6 @@ terraform {
 
 provider "aws" {
   region     = var.AWS_REGION
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
-
+  access_key = "${secrets.AWS_ACCESS_KEY}"
+  secret_key = "${secrets.AWS_SECRET_KEY}"
 }
